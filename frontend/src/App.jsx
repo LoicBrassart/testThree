3058d0a07f9655e3d563617a2702b8b0c9029707
LoicBrassart <loic.brassart@gmail.com>
2022-09-21 17:30:10 +0200

    First steps

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,31 @@
-import Home from "@pages/Home";
-
-import "./App.css";
+import { useEffect, useState } from "react";
+import * as Three from "three";
 
 function App() {
+  const [scene, setScene] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [renderer, setRenderer] = useState(null);
+
+  useEffect(() => {
+    setScene(new Three.Scene());
+    setCamera(
+      new Three.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      )
+    );
+
+    const newRenderer = new Three.WebGLRenderer();
+    newRenderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+    console.log(newRenderer.domElement);
+    setRenderer(newRenderer);
+    // document.body.appendChild(renderer.domElement);
+  }, []);
+
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <div className="App">Hey Three !{renderer && renderer.domElement}</div>
   );
 }
 
